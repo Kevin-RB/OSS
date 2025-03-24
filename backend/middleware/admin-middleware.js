804adad5cd6073c58335864a3ce62405a7a864e1
Kevin Romero <kr38996@gmail.com>
2025-03-24 18:46:52 +1000

    Add admin verification middleware and secure product routes

---
+++ backend/middleware/admin-middleware.js
@@ -0,0 +1,17 @@
+
+const verifyAdmin = async (req, res, next) => {
+    try {
+        const user = req.user;
+        if(user?.role === 'admin') {
+            next();
+        }
+        else {
+            res.status(401).json({ message: 'Not authorized as an admin' });
+        }
+    }catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+};
+
+module.exports = { verifyAdmin };
--- backend/routes/product-routes.js
+++ backend/routes/product-routes.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const { createProduct, deleteProduct, getProductById, getProducts, updateProductById, getProductByName } = require('../controllers/product-controller');
 const { protect } = require('../middleware/authMiddleware');
+const { verifyAdmin } = require('../middleware/admin-middleware');
 const router = express.Router();
 
 router.get('/', protect, getProducts);
 router.get('/:productId', protect, getProductById);
 router.get('/:productName', protect, getProductByName);
-router.post('/', protect, createProduct);
-router.put('/:productId', protect, updateProductById);
-router.delete('/:productId', protect, deleteProduct);
-
+router.post('/', protect, verifyAdmin, createProduct);
+router.put('/:productId', protect, verifyAdmin, updateProductById);
+router.delete('/:productId', protect, verifyAdmin, deleteProduct);
 
 module.exports = router;
