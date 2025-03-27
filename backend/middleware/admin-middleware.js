a16b8db97ba131c35d7a122884ceac9520819463
Kevin Romero <kr38996@gmail.com>
2025-03-27 14:17:45 +1000

    admin middleware comment

--- backend/middleware/admin-middleware.js
+++ backend/middleware/admin-middleware.js
@@ -1,6 +1,7 @@
 
 const verifyAdmin = async (req, res, next) => {
     try {
+        // from the user object in the request, check if the user is an admin
         const user = req.user;
         if(user?.role[0] === 'admin') {
             next();
