b0d958329f4e6058ae593f3d7b2373e4fd6c60a1
GaheeKIM <gaheekim610@gmail.com>
2025-05-14 01:35:29 +1000

    create basic ui components and test on the login page

---
+++ frontend/src/components/ui/label.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { cn } from "../../lib/utils";
+
+export const Label = React.forwardRef(({ className, ...props }, ref) => {
+  return (
+    <label
+      ref={ref}
+      className={cn("text-sm font-medium", className)}
+      {...props}
+    />
+  );
+});
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,26 +1,79 @@
-import { useState } from 'react';
-import { useAuth } from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
-import { axiosInstance } from '../axiosConfig';
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../axiosConfig";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
+import { Button } from "../components/ui/button";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
 
 const Login = () => {
-  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [formData, setFormData] = useState({ email: "", password: "" });
   const { login } = useAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axiosInstance.post('/api/auth/login', formData);
+      const response = await axiosInstance.post("/api/auth/login", formData);
       login(response.data);
-      navigate('/product');
+      navigate("/product");
     } catch (error) {
-      alert('Login failed. Please try again.');
+      alert("Login failed. Please try again.");
     }
   };
 
   return (
     <div className="max-w-md mx-auto mt-20">
+      <Button size="sm" disabled>
+        Small
+      </Button>
+      <Button disabled>Default</Button>
+      <Button size="lg" disabled>
+        Large
+      </Button>
+      <Button variant="destructive">shadcn/ui</Button>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <div className="grid w-full items-center gap-2">
+        <Label htmlFor="error">Your Name</Label>
+        <Input
+          type="text"
+          id="error"
+          placeholder="Enter your name"
+          className="border-red-600"
+        />
+        <p className="text-xs font-medium text-red-600">
+          This field is required
+        </p>
+      </div>
+
       <form onSubmit={handleSubmit} className="bg-white p-6 shadow-md rounded">
         <h1 className="text-2xl font-bold mb-4 text-center">Login</h1>
         <input
@@ -34,10 +87,15 @@ const Login = () => {
           type="password"
           placeholder="Password"
           value={formData.password}
-          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
           className="w-full mb-4 p-2 border rounded"
         />
-        <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded">
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white p-2 rounded"
+        >
           Login
         </button>
       </form>
