cd671d69fa6ea3b11af7ec67d6af85b65b109b54
GaheeKIM <gaheekim610@gmail.com>
2025-05-15 15:17:27 +1000

    remove comments

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -26,7 +26,6 @@ const Register = () => {
   const onSubmit = async (data) => {
     try {
       await axiosInstance.post("/api/auth/register", data);
-      // alert("Registration successful. Please log in.");
       // Show success toast
       addToast({
         title: "Success",
@@ -41,7 +40,6 @@ const Register = () => {
         error.response &&
         error.response.data
       ) {
-        // setError(error.response.data?.error);
         console.log(error);
         addToast({
           title: "Error",
@@ -58,7 +56,6 @@ const Register = () => {
         variant: "error",
         duration: 3000,
       });
-      // alert("Registration failed. Please try again.");
     }
   };
 
@@ -132,50 +129,6 @@ const Register = () => {
         </form>
       </div>
     </div>
-    // <div className="max-w-md mx-auto mt-20">
-    //   <form onSubmit={handleSubmit} className="bg-white p-6 shadow-md rounded flex flex-col space-y-4">
-    //     <h1 className="text-2xl font-bold mb-4 text-center">Register</h1>
-    //     <div>
-    //       <input
-    //         type="text"
-    //         placeholder="Name"
-    //         value={formData.name}
-    //         onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-    //         aria-errormessage='Name error'
-    //         className="w-full p-2 border rounded"
-    //       />
-    //       <FieldError error={error?.["name"]?._errors} />
-    //     </div>
-
-    //     <div>
-    //       <input
-    //         type="email"
-    //         placeholder="Email"
-    //         value={formData.email}
-    //         onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-    //         aria-errormessage='Email error'
-    //         className="w-full p-2 border rounded"
-    //       />
-    //       <FieldError error={error?.["email"]?._errors} />
-    //     </div>
-
-    //     <div>
-    //       <input
-    //         type="password"
-    //         placeholder="Password"
-    //         value={formData.password}
-    //         onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-    //         aria-errormessage='Password error'
-    //         className="w-full p-2 border rounded"
-    //       />
-    //       <FieldError error={error?.["password"]?._errors} />
-    //     </div>
-
-    //     <button type="submit" className="w-full bg-green-600 text-white p-2 rounded">
-    //       Register
-    //     </button>
-    //   </form>
-    // </div>
   );
 };
 
